966b72ad6914adb42f9ff539c6c32cc510067249
Satyam Kumar <satyamkumar629301@gmail.com>
2026-01-11 14:59:04 +0530

    fix show error

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -4,26 +4,30 @@ import "./SearchBox.css"
 import { useState } from 'react';
 
 export default function SearchBox({ updateInfo }) {
-    let [city ,setCity] = useState("");
-    let [error ,seterror] = useState(false);
+
+    let [city, setCity] = useState("");
+
+    let [error, seterror] = useState(false);
+
     const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+
     const API_KEY = "cbe0339970196f8d1590cf8958eeb1a2";
 
     let getWeather = async ()=>{
         try {
-        let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
-        let data = await response.json();
-        let result = {
-            city : city,
-            temp : data.main.temp,
-            temp_min : data.main.temp_min,
-            temp_max : data.main.temp_max,
-            humidity : data.main.humidity,
-            feels_like : data.main.feels_like,
-            weather : data.weather[0].description,
-        };
-        console.log(result);
-        return result;
+            let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+            let data = await response.json();
+            let result = {
+                city : city,
+                temp : data.main.temp,
+                temp_min : data.main.temp_min,
+                temp_max : data.main.temp_max,
+                humidity : data.main.humidity,
+                feels_like : data.main.feels_like,
+                weather : data.weather[0].description,
+            };
+            console.log(result);
+            return result;
         } catch(error) {
             throw(error)
         }
@@ -35,9 +39,10 @@ export default function SearchBox({ updateInfo }) {
     };
     let handleSubmit = async (event)=>{
         try {
-        event.preventDefault();
-        let info = await getWeather();
-        updateInfo(info);
+            event.preventDefault();
+            let info = await getWeather();
+            updateInfo(info);
+            seterror(false);
         } catch(err) {
             seterror(true);
         }
